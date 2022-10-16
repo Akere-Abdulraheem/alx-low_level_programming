9ae1b9381216c2407065b39a845a6236cd515b4d
Akere-Abdulraheem <akereodunayo@gmail.com>
2022-10-16 14:19:35 -0700

    created file 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 
 int main(void)
 {
-	puts(""Programming is like building a multilingual puzzle "\n")
-		Return : (0)
+	puts(" " 'Programming is like building a multilingual puzzle' "\n");
+		Return : (0);
 }
 
