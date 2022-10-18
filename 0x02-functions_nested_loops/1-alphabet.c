e07600d01732840a228c48c97c5082e5f4c0b6e2
Akere-Abdulraheem <akereodunayo@gmail.com>
2022-10-18 12:36:51 -0700

    created file 2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main -entry point
+ *
+ * char == character variable
+ *
+ * for == a for loop where ch == a and keeps incrementing untill it == to z
+ *
+ * putchar == print ch
+ *
+ * Return value 0sucess
+ */
+
+
+int main () {
+
+	   char ch;
+
+
+
+	      for(ch = 'a' ; ch <= 'z' ; ch++) {
+
+		            putchar(ch);
+
+			       }
+
+	         
+
+	         return(0);
+
+}
