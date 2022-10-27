2ae675ef64973e052fa55085a58db63982a93736
Akere-Abdulraheem <akereodunayo@gmail.com>
2022-10-27 14:45:42 -0700

    included #include <stdlib.h> in file 1

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main -Entry point
+ *
+ * Print a random number and tells if it last digit is postive or not
+ *
+ * Return: Always 0 (sucess)
+ */
+
+int main(void)
+
+{
+
+		int n;
+
+
+
+			srand(time(0));
+
+				n = rand() - RAND_MAX / 2;
+
+
+				if (n > 5)
+				{
+					printf("last digit of %d is %d and is greater than 5\n", n, n % 10);
+				}
+				else if (n == 0)
+				{
+					printf("last digit of %d is %d and is 0\n", n, n % 10);
+				}
+				else
+				{
+					printf("last digit of %d is %d and is less than 6 and not 0\n", n, n %
+							10);
+				}
+
+				return (0);
+
+}
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main -entry point
+ *
+ * a code that print alphabet in lowercase using putchar
+ *
+ * Return: Always 0 (sucess)
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = "a"; ch <= "z"; ch++;)
+		putchar(ch);
+
+Return: 0 ;
+}
