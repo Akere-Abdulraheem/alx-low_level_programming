458358bdccb3ccc89e6a44839e848f95800486f7
Akere-Abdulraheem <akereodunayo@gmail.com>
2022-10-27 15:07:14 -0700

    added another if else to file 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -31,10 +31,14 @@ int main(void)
 				{
 					printf("last digit of %d is %d and is 0\n", n, n % 10);
 				}
-				else
+				else if (n < 6 != 0)
 				{
 					printf("last digit of %d is %d and is less than 6 and not 0\n", n, n %
 							10);
+				}
+				else
+				{
+					printf("nothing")
 				}
 
 				return (0);
