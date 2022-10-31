c7e7ca5e8cada5e60f1668ff59f86a5e7bc07f78
Akere-Abdulraheem <akereodunayo@gmail.com>
2022-10-31 14:01:16 -0700

    created file 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main -entry point
+ *
+ * a code that print alphabet in lowercase in reverse order using putchar
+ *
+ * Return: Always 0 (sucess)
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch <= 'a'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+
+return (0);
+}
