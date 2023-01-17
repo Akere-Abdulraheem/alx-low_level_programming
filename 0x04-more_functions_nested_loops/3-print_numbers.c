4dde188a32c62b31cebb023bd9a7b400c671a22f
Akere-Abdulraheem <akereodunayo@gmail.com>
2023-01-17 14:32:49 +0000

    putchargit add .git add .

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - a function that prints numbers from 0 to 9.
+ *
+ * Return: multiplicatin of a and b
+ */
+
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 48; c <= 57; c++)
+	{
+		putchar(c);
+	}
+		putchar('\n');
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,1 +1,2 @@
 #More on Functions and nested loops
+To add a new line with putchar...you write * putchar('\n') *
