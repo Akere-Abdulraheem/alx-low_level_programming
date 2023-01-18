03388dcbe8cfbfd6993b7256d6556505f8030ee3
Akere-Abdulraheem <akereodunayo@gmail.com>
2023-01-18 21:42:31 +0000

    error fixed after 3hrs+...file 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -5,10 +5,19 @@
  * @a: First integer
  * @b: Second integer
  *
+ * Ok so what I did was to save the value of a first in the variable "h"
+ * before it gets overwritten using pointer d, then I swapped the value
+ * of a to b using the pointer f, and what remains is to called the stored
+ * value in "h"
+ *
  * Return: nothing void.
  */
 void swap_int(int *a, int *b)
 {
-	*a = 42;
-	*b = 98;
+	int *d = a;
+	int h = *d;
+	int *f = b;
+
+	*a = *f;
+	*b = h;
 }
