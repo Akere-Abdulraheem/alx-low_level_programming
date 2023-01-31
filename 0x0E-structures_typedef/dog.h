c84f3ff18f15481419b0e31c80d5b2d51bb568a7
Akere-Abdulraheem <akereodunayo@gmail.com>
2023-01-31 12:11:52 +0000

    ...

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,6 +3,10 @@
 
 #include <stdio.h>
 
+/**
+ * dod_t - new name for struct dog
+ */
+typedef struct dog dog_t
 
 /**
  * struct dog  - stores info for dog
@@ -17,7 +21,6 @@ struct dog
 	char *owner;
 };
 
-typedef struct dog dog_t
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
