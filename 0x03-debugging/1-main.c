8b32e7c7e6ad5d503c6d94e5fa3b6f89fb9b6258
Akere-Abdulraheem <akereodunayo@gmail.com>
2023-03-29 23:02:42 +0000

    corrected code - commented out the while loop

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,10 +13,10 @@ int main(void)
 	
 	i = 0;
 	
-	while (i < 10)
+	/*while (i < 10)
         {
-                putchar(i); i++;/*There is no incrementation i.e i++*/
-        }
+                putchar(i);
+	}*/
 
         printf("Infinite loop avoided! \\o/\n");
 
