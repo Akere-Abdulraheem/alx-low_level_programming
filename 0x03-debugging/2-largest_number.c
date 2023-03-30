80d9a588ae5d287d74cda909b3b9e82c94cbfe59
Akere-Abdulraheem <akereodunayo@gmail.com>
2023-03-30 00:03:40 +0000

    fixed bug and compiled program

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,7 +16,7 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	}
-	else if (b > a && c > a) /* (b > a && b > c) */
+	else if (a > b && c > a) /* (b > a && b > c) */
 	{
 		largest = c;
 	}
